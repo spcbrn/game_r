72b0f22d37bba32ee3dc46464dbb8655457a946b
Christopher Lemke <spcbrn1@gmail.com>
2018-10-14 16:03:18 -0600

    first, grid rendering, tiles randomized

---
+++ src/GameInterface.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import GameCanvas from './components/GameCanvas';
+
+class GameInterface extends Component {
+  render() {
+    return (
+      <div style={{ width: '1280px', height: '1024px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <GameCanvas />
+      </div>
+    );
+  }
+}
+
+export default GameInterface;
